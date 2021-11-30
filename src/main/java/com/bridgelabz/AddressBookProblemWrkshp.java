b6a90b64b6fd81bfedf5b2628fb5437d6644eb53
GeetaSMath <geetasmath@gmail.com>
2021-11-30 08:29:31 +0530

    Added:welcome message to solve addressbook problem

---
+++ src/main/java/com/bridgelabz/AddressBookProblemWrkshp.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class AddressBookProblemWrkshp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to solve addressBook Problem");
+    }
+}
